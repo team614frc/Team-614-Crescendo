d18592f39cabc4e940efa0e7157783b39655fed7
zabzzz <121322821+zabzzz@users.noreply.github.com>
2024-02-06 13:52:38 -0800

    organize
    organized pivot code

--- src/main/java/frc/robot/commands/manipulator/pivot/PivotDown.java
+++ src/main/java/frc/robot/commands/manipulator/pivot/PivotDown.java
@@ -2,7 +2,7 @@
 // // Open Source Software; you can modify and/or share it under the terms of
 // // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.manipulator;
+package frc.robot.commands.manipulator.pivot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
