9ef6379f701e9a0b372c7011266a36354a79ba74
zabzzz <121322821+zabzzz@users.noreply.github.com>
2024-03-04 14:10:32 -0800

    180 gear ratio

--- src/main/java/frc/robot/subsystems/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -90,7 +90,7 @@ public class PivotSubsystem extends ProfiledPIDSubsystem {
   }
 
   public double getEncoderinDegrees() {
-    double val = 300/360.0;  // 7168 ticks per rev, 100:1 gear ratio, ticks per full rotation in degrees
+    double val = 180.0/360.0;  // 7168 ticks per rev, 100:1 gear ratio, ticks per full rotation in degrees
     return (getPivotLEncoder()/val);
   }
 
