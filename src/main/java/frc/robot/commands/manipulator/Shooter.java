d873277ac8f3cd586c4b5c553e1aceecfe1ae72a
team614robotics <121322821+zabzzz@users.noreply.github.com>
2024-03-05 13:22:05 -0500

    boom set feedr

--- src/main/java/frc/robot/commands/manipulator/Shooter.java
+++ src/main/java/frc/robot/commands/manipulator/Shooter.java
@@ -5,11 +5,10 @@
 package frc.robot.commands.manipulator;
 
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
 import frc.robot.Constants.ManipulatorConstants;
-import frc.robot.Constants.TimeConstants;
+//import frc.robot.Constants.TimeConstants;
 
 public class Shooter extends Command {
 
@@ -21,7 +20,7 @@ public class Shooter extends Command {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(RobotContainer.shooterSubsystem);
     addRequirements(RobotContainer.intakeSubsystem);
-    commandTimer = new Timer();
+    //commandTimer = new Timer();
     this.shootSpeed = shootSpeed;
   }
 
@@ -29,14 +28,14 @@ public class Shooter extends Command {
   @Override
   public void initialize() {
     RobotContainer.shooterSubsystem.enable();
-    commandTimer.restart();
+    //commandTimer.restart();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     RobotContainer.shooterSubsystem.setSetpoint(shootSpeed);
-    if (commandTimer.get() > TimeConstants.SpeakerFeed) {
+    if (RobotContainer.shooterSubsystem.getMeasurement() >= shootSpeed - 150) {
       RobotContainer.intakeSubsystem.setFeed(ManipulatorConstants.LOADING_SPEED);
     }
   }
@@ -51,6 +50,6 @@ public class Shooter extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return commandTimer.get() > TimeConstants.SpeakerEnd;
+    return RobotContainer.shooterSubsystem.getMeasurement() >= shootSpeed - 100;
   }
 }
